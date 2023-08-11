069a1f856907742dc8aeaa386a637e695feb18f3
MsN <mohsen.s7s.ab@gmail.com>
2023-08-11 14:27:29 +0330

    store and test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -14,13 +14,14 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	testDB, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to database: ", err)
 	}
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
