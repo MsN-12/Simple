74a55f7a064da53868eb6d50c317ac54a196eb1b
MsN <mohsen.s7s.ab@gmail.com>
2023-08-21 11:00:11 +0330

    add user and test and handle errors

--- api/account.go
+++ api/account.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	db "github.com/MsN-12/simpleBank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	"log"
 	"net/http"
 )
 
@@ -25,6 +27,9 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	}
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			log.Println(pqErr.Code.Name())
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
