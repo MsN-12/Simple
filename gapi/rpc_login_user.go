a962183076599b8d71caafa6d02ee76c2689cf04
MsN <mohsen.s7s.ab@gmail.com>
2023-10-11 18:51:06 +0330

    extract meta data. set client ip and user agent

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -41,12 +41,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token")
 	}
 
+	mtdt := server.extractMetadata(ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.ClientIP,
+		ClientIp:     mtdt.UserAgent,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
