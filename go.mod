42b5c82e163dc6dfe7e0819cb4b4fa5d71a2246e
MsN <mohsen.s7s.ab@gmail.com>
2023-11-16 11:27:12 +0330

    implement email sender and the test

--- go.mod
+++ go.mod
@@ -39,6 +39,7 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hibiken/asynq v0.24.1 // indirect
+	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
---
+++ mail/sender_test.go
@@ -0,0 +1,30 @@
+package mail
+
+import (
+	"testing"
+
+	"github.com/MsN-12/simpleBank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSendEmailWithGmail(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	config, err := util.LoadConfig("..")
+	require.NoError(t, err)
+
+	sender := NewGmailSender(config.EmailSenderName, config.EmailSenderAddress, config.EmailSenderPassword)
+
+	subject := "A test email"
+	content := `
+	<h1>Hello world</h1>
+	<p>This is a test message from Simple Bank</p>
+	`
+	to := []string{"mohsen.s7s.ab@gmail.com"}
+	attachFiles := []string{"../README.md"}
+
+	err = sender.SendEmail(subject, content, to, nil, nil, attachFiles)
+	require.NoError(t, err)
+}
