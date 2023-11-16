30cd6432712eafa1190f8fcd2ba35b017c24bbf4
MsN <mohsen.s7s.ab@gmail.com>
2023-11-16 10:58:18 +0330

    add logger for asynq errors

--- worker/processor.go
+++ worker/processor.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	db "github.com/MsN-12/simpleBank/db/sqlc"
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -27,6 +28,11 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 			QueueCritical: 10,
 			QueueDefault:  5,
 		},
+		ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+			log.Error().Err(err).Str("type", task.Type()).
+				Bytes("payload", task.Payload()).Msg("process task failed")
+		}),
+		Logger: NewLogger(),
 	})
 	return &RedisTaskProcessor{
 		server: server,
